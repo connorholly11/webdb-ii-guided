8dd4858d97279ab65683365ea04804516a7f7d2d
Connor Holly <connorholly11@gmail.com>
2019-10-08 13:36:14 -0400

    ...

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    //DB DRIVER
+    client: "sqlite3",
+    connection: {
+      filename: "./data/produce.db3"
+    },
+    useNullAsDefault: true
+  },
+
+  staging: {
+    client: "postgresql",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  },
+
+  //defining the required configuration
+  //knex needs to connect to production
+  production: {
+    client: "pg",
+    connection: {
+      database: "my_db",
+      user: "username",
+      password: "password"
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: "knex_migrations"
+    }
+  }
+};
